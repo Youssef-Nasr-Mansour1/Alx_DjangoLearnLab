c5382221a3eb92d4f51cb58cc7a3043a52ceea72
Youssef-Nasr-Mansour <youssefnasr852@gmail.com>
2024-11-09 14:21:46 +0200

    Implementing User Authentication in Django

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,6 +5,13 @@ from .views import list_books, LibraryDetailView
 urlpatterns = [
     path('books/', list_books, name='list_books'),  # Function-based view URL
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view URL
+]
+
+# relationship_app/urls.py
+from django.urls import path
+from .views import register, user_login, user_logout
+
+urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
     path('register/', register, name='register'),
