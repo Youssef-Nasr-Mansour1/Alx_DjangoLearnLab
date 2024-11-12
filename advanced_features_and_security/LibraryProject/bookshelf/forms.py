374578c715e133ff48c13ce1e65db90c9209e473
Youssef-Nasr-Mansour <youssefnasr852@gmail.com>
2024-11-13 00:07:30 +0200

    Implementing Security Best Practices in Django

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,3 +2,15 @@ from django import forms
 
 class SearchForm(forms.Form):
     query = forms.CharField(max_length=100, required=True)
+
+# bookshelf/forms.py
+from django import forms
+from .models import Book
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'description']
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
+        }
