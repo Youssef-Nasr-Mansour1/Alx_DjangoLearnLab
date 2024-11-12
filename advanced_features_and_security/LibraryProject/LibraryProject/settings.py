b8d73df77d7c037fb7455d68491dc977dd86cd8a
Youssef-Nasr-Mansour <youssefnasr852@gmail.com>
2024-11-12 23:15:52 +0200

    Implementing a Custom User Model in Django

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'book_store.apps.BookStoreConfig',
-    'relationship_app',
+    'relationship_app'
+    'bookshelf,
 ]
 
 MIDDLEWARE = [
