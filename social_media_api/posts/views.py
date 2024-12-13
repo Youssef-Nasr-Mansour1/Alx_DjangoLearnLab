573d61fe63a0a33deba4911eeebb63a39a747557
Youssef-Nasr-Mansour <youssefnasr852@gmail.com>
2024-12-14 01:28:29 +0200

    Implementing User Follows and Feed Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions.IsAuthenticated import IsAuthenticated
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import Post, Like, Comment
